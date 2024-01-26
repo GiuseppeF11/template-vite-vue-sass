dec595f1cfb66a0a6314e9a08ffbeef7065e74c8
GiuseppeF11 <faillagiuseppe13@gmail.com>
2024-01-26 10:43:40 +0100

    Aggiunto font awesome

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import App from './App.vue'
 import * as bootstrap from 'bootstrap'
 
 
-createApp(App).mount('#app')
+/* import { library } from '@fortawesome/fontawesome-svg-core'
+
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faUserSecret) */
+
+
+createApp(App)
+/* .component('font-awesome-icon', FontAwesomeIcon) */
+.mount('#app')
